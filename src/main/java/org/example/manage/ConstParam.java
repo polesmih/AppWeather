75e24a88c2a1d1a7235b26fc082d9be79f332876
polesmih <polesmih@yandex.ru>
2023-02-28 09:08:44 +0300

    Оптимизирован код, проект подготовлен для сборки

--- src/main/java/org/example/manage/ConstParam.java
+++ src/main/java/org/example/manage/ConstParam.java
@@ -4,6 +4,10 @@ public class ConstParam {
 
     public static final String NOT_FOUND = "Город с таким названием не найден";
     public static final String ENTER_CITY = "Нужно ввести название города";
+    public static final String HELP = "Название города можно вводить на русском " +
+            "либо на английском языке.\n" +
+            "Для уточнения страны, через запятую необходимо ввести ее аббревитуру по-английски.\n" +
+            "Например: рим, it";
     public static final String DEGREE = " \u2103";
     public static final String SPEED = " м/с";
     public static final String SCALE = " мм.рт.ст";
